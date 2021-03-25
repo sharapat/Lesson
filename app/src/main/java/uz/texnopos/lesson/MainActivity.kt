f1c8a7c35fb5309eb226b3c5c3f74783a03d3ebf
Sharapat Kalabaev <kalabaevshj@gmail.com>
2021-03-25 19:36:23 +0500

    ameller

--- app/src/main/java/uz/texnopos/lesson/MainActivity.kt
+++ app/src/main/java/uz/texnopos/lesson/MainActivity.kt
@@ -1,61 +1,40 @@
 package uz.texnopos.lesson
 
-import android.content.Context
-import android.content.SharedPreferences
-import android.media.MediaPlayer
-import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
-import android.widget.*
+import android.view.View
+import android.widget.Button
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
-    lateinit var mediaPlayer: MediaPlayer
-    lateinit var sharedPreferences: SharedPreferences
-    private var position = 0
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        sharedPreferences = getSharedPreferences(this.packageName, Context.MODE_PRIVATE)
-        Log.d("ómirlik cikli", "onCreate")
+        setContentView(R.layout.activity_main)
+        buttonPlus.setOnClickListener {
+            if (et1.text.isNotEmpty() && et2.text.isNotEmpty()) {
+                tvResult.text = "${et1.text.toString().toInt()+et2.text.toString().toInt()}"
+            }
+        }
     }
 
-    override fun onStart() {
-        super.onStart()
-        val uri = Uri.parse("android.resource://" + packageName + "/" + R.raw.audio)
-        mediaPlayer = MediaPlayer.create(this, uri)
-        position = sharedPreferences.getInt("position", 0)
-        mediaPlayer.seekTo(position)
-        mediaPlayer.start()
-        Log.d("ómirlik cikli", "onStart")
-    }
-
-    override fun onResume() {
-        super.onResume()
-        Log.d("ómirlik cikli", "onResume")
-    }
-
-    override fun onRestart() {
-        super.onRestart()
-        Log.d("ómirlik cikli", "onRestart")
-    }
-
-    override fun onPause() {
-        super.onPause()
-        sharedPreferences.edit().putInt("position", mediaPlayer.currentPosition).apply()
-        mediaPlayer.stop()
-        Log.d("ómirlik cikli", "onPause")
-    }
-
-    override fun onStop() {
-        super.onStop()
-        Log.d("ómirlik cikli", "onStop")
-    }
-
-    override fun onDestroy() {
-        super.onDestroy()
-        sharedPreferences.edit().putInt("position", 0).apply()
-        Log.d("ómirlik cikli", "onDestroy")
+    fun onClick(view: View) {
+        if (et1.text.isNotEmpty() && et2.text.isNotEmpty()) {
+            when((view as Button).text) {
+                "+" -> {
+                    tvResult.text = "${et1.text.toString().toInt()+et2.text.toString().toInt()}"
+                }
+                "-" -> {
+                    tvResult.text = "${et1.text.toString().toInt()-et2.text.toString().toInt()}"
+                }
+                "*" -> {
+                    tvResult.text = "${et1.text.toString().toInt()*et2.text.toString().toInt()}"
+                }
+                "/" -> {
+                    tvResult.text = "${et1.text.toString().toDouble()/et2.text.toString().toDouble()}"
+                }
+            }
+        }
     }
 }
--- app/src/main/java/uz/texnopos/lesson/SecondActivity.kt
+++ app/src/main/java/uz/texnopos/lesson/SecondActivity.kt
@@ -2,12 +2,10 @@ package uz.texnopos.lesson
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import kotlinx.android.synthetic.main.activity_game_over.*
 
-class GameOverActivity : AppCompatActivity() {
+class SecondActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_game_over)
-        val ball = intent.getIntExtra("ball", 0)
+        setContentView(R.layout.activity_second)
     }
 }
